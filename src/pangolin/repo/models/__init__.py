e96fff8233bbe17238209b650c1dd39ea6995e80
navid <navidsoleymani@ymail.com>
2023-07-25 10:29:57 +0330

    fixed bugs

--- src/pangolin/repo/models/__init__.py
+++ src/pangolin/repo/models/__init__.py
@@ -4,23 +4,35 @@ from .__log import Log as LogModel
 from .__response import Response as ResponseModel
 from .__response_property import ResponseProperty as ResponsePropertyModel
 
-__model_list = [RequestModel, RequestPropertyModel, LogModel, ResponseModel, ResponsePropertyModel]
-__model_name_list = [c.__class__.__name__ for c in __model_list]
-
 
 class LoggerDBRouter(object):
+    DEFAULT = 'default'
+    LOGGER_DB = 'logger_db'
+
+    @property
+    def __model_list(self):
+        ret = [
+            RequestModel,
+            RequestPropertyModel,
+            LogModel,
+            ResponseModel,
+            ResponsePropertyModel,
+        ]
+        return ret
+
+    @property
+    def __model_name_list(self):
+        ret = [c.__class__.__name__ for c in self.__model_list]
+        return ret
 
     def db_for_read(self, model, **hints):
-        if model in [RequestModel, RequestPropertyModel, LogModel, ResponseModel, ResponsePropertyModel]:
-            return 'logger_db'
-        return 'default'
+        ret = self.LOGGER_DB if model in self.__model_list else self.DEFAULT
+        return ret
 
     def db_for_write(self, model, **hints):
-        if model in [RequestModel, RequestPropertyModel, LogModel, ResponseModel, ResponsePropertyModel]:
-            return 'logger_db'
-        return 'default'
+        ret = self.LOGGER_DB if model in self.__model_list else self.DEFAULT
+        return ret
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if model_name in __model_name_list:
-            return ('logger_db' == db)
-        return ('default' == db)
+        ret = self.LOGGER_DB == db if model_name in self.__model_name_list else self.DEFAULT == db
+        return ret
